ec90ce87294d8ff885807b047c5d6e06b96feeab
tedglim <tedlim@alumni.stanford.edu>
2021-04-03 15:17:20 -0700

    Untrack files in .gitignore

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -36,7 +36,7 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
